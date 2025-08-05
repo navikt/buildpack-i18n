3a6aafaa019b33a2cadd923275e0a672a0d02889
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-08-05 12:53:40 +0200

    chore

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,11 +23,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
